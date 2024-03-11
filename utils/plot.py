0eed22972db53115a8cc7bc90b23db0557adc995
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-03-11 09:36:28 +0300

    llm analysis

--- utils/plot.py
+++ utils/plot.py
@@ -4,8 +4,8 @@ import numpy as np
 import re
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
-from sklearn.metrics import silhouette_score
+from sklearn.manifold import TSNE
+from umap import UMAP
 
 def find_elbow_point(x, y):
     """
@@ -15,26 +15,6 @@ def find_elbow_point(x, y):
     ddy = [dy[i] - dy[i-1] for i in range(1, len(dy))]
     elbow_index = ddy.index(max(ddy)) + 1
     return x[elbow_index]
-
-def silhouette_to_find_optimal_k(job_embeddings):    
-    k_values = range(2, 11)
-    silhouette_scores = []
-    
-    for k in k_values:
-        kmeans = KMeans(n_clusters=k, random_state=42, n_init=10)
-        cluster_label = kmeans.fit_predict(job_embeddings)
-        silhouette_avg = silhouette_score(job_embeddings, cluster_label)
-        silhouette_scores.append(silhouette_avg)
-
-    plt.plot(k_values, silhouette_scores, marker='o')
-    plt.xlabel('Number of Clusters (K)')
-    plt.ylabel('Silhouette Score')
-    plt.title('Silhouette Score for Optimal K (using BERT)')
-    plt.xticks(k_values)
-    plt.show()
-    
-    optimal_k = k_values[np.argmax(silhouette_scores)]
-    return optimal_k
 
 def calculate_matched_percentages(level_df, cluster_labels, decimal_places=2):
     percentages = {}
@@ -53,6 +33,110 @@ def calculate_matched_percentages(level_df, cluster_labels, decimal_places=2):
     
     return percentages
 
+#  UMAP VISUALIZATION
+def visualize_with_umap_2d(level_embeddings, cluster_labels, level_df):
+    umap_2d = UMAP(n_components=2, init='random', random_state=0)
+    embeddings_2d = umap_2d.fit_transform(level_embeddings)
+
+    percentages = calculate_matched_percentages(level_df, cluster_labels)
+    plt.figure(figsize=(8, 6))
+    for label in np.unique(cluster_labels):
+        if label in percentages:
+            label_text = '\n'.join([f'{key}: {value}%' for key, value in percentages[label].items()])
+            plt.scatter(embeddings_2d[cluster_labels == label, 0], embeddings_2d[cluster_labels == label, 1],
+                            label=label_text)
+        else:
+            label_text = f'Cluster {label}'
+            plt.scatter(embeddings_2d[cluster_labels == label, 0], embeddings_2d[cluster_labels == label, 1],
+                            label=label_text)
+
+    plt.title('Clustering Visualization')
+    plt.xlabel('PCA Component 1')
+    plt.ylabel('PCA Component 2')
+    plt.legend()
+    plt.xticks(rotation='vertical')    
+    plt.show()
+
+def visualize_with_umap_3d(level_embeddings, cluster_labels, level_df):
+    umap_2d = UMAP(n_components=3, init='random', random_state=0)
+    embeddings_3d = umap_2d.fit_transform(level_embeddings)
+
+    percentages = calculate_matched_percentages(level_df, cluster_labels)
+
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    
+    for label in np.unique(cluster_labels):      
+        if label in percentages:
+            label_text = '\n'.join([f'{key}: {value}%' for key, value in percentages[label].items()])
+            ax.scatter(embeddings_3d[cluster_labels == label, 0], embeddings_3d[cluster_labels == label, 1], embeddings_3d[cluster_labels == label, 2],
+                    label=label_text)
+        else:
+            label_text = f'Cluster {label}'
+            ax.scatter(embeddings_3d[cluster_labels == label, 0], embeddings_3d[cluster_labels == label, 1], embeddings_3d[cluster_labels == label, 2],
+                    label=label_text)
+    ax.set_title('Clustering Visualization (3D)')
+    ax.set_xlabel('PCA Component 1')
+    ax.set_ylabel('PCA Component 2')
+    ax.set_zlabel('PCA Component 3')
+    ax.legend()
+
+    plt.show()
+
+
+#  TSNE VISUALIZATIOIN
+def visualize_with_tsne_2d(level_embeddings, cluster_labels, level_df):
+    level_embeddings = np.array(level_embeddings)  
+    tsne = TSNE(n_components=2, perplexity=5, random_state=0)  
+    embeddings_2d = tsne.fit_transform(level_embeddings)
+
+    percentages = calculate_matched_percentages(level_df, cluster_labels)
+
+    plt.figure(figsize=(8, 6))
+    for label in np.unique(cluster_labels):
+        if label in percentages:
+            label_text = '\n'.join([f'{key}: {value}%' for key, value in percentages[label].items()])
+            plt.scatter(embeddings_2d[cluster_labels == label, 0], embeddings_2d[cluster_labels == label, 1],
+                            label=label_text)
+        else:
+            label_text = f'Cluster {label}'
+            plt.scatter(embeddings_2d[cluster_labels == label, 0], embeddings_2d[cluster_labels == label, 1],
+                            label=label_text)
+
+    plt.title('Clustering Visualization')
+    plt.xlabel('TSNE Component 1')
+    plt.ylabel('TSNE Component 2')
+    plt.legend()
+    plt.xticks(rotation='vertical')    
+    plt.show()
+
+def visualize_with_tsne_3d(level_embeddings, cluster_labels, level_df):
+    level_embeddings = np.array(level_embeddings)  # Convert to NumPy array
+    tsne = TSNE(n_components=3, perplexity=5, random_state=0)  # Adjust perplexity as needed
+    embeddings_3d = tsne.fit_transform(level_embeddings)
+
+    percentages = calculate_matched_percentages(level_df, cluster_labels)
+
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    for label in np.unique(cluster_labels):
+        if label in percentages:
+            label_text = '\n'.join([f'{key}: {value}%' for key, value in percentages[label].items()])
+            ax.scatter(embeddings_3d[cluster_labels == label, 0], embeddings_3d[cluster_labels == label, 1],
+                       embeddings_3d[cluster_labels == label, 2], label=label_text)
+        else:
+            label_text = f'Cluster {label}'
+            ax.scatter(embeddings_3d[cluster_labels == label, 0], embeddings_3d[cluster_labels == label, 1],
+                       embeddings_3d[cluster_labels == label, 2], label=label_text)
+
+    ax.set_title('Clustering Visualization')
+    ax.set_xlabel('TSNE Component 1')
+    ax.set_ylabel('TSNE Component 2')
+    ax.set_zlabel('TSNE Component 3')
+    ax.legend()
+    plt.show()
+
+#  PCA VISUALIZATION
 def visualize_clusters(level_embeddings, cluster_labels, level_df):
     pca = PCA(n_components=2, random_state=42)
     embeddings_2d = pca.fit_transform(level_embeddings)
--- utils/util.py
+++ utils/util.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import re
+from sklearn.cluster import KMeans
+from sklearn.metrics import silhouette_score
 from openai import OpenAI
 from langchain.embeddings.openai import OpenAIEmbeddings
 from dotenv import dotenv_values
@@ -15,7 +17,26 @@ def embedding(samples):
     embedded_documents = embed_model.embed_documents(samples)
     return embedded_documents
 
-def Fetch_Each_clustering_Content(optimal_k, texts, cluster_labels):
+def silhouette_to_find_optimal_k(level_embeddings):    
+    k_values = range(2, 11)
+    silhouette_scores = []
+    
+    for k in k_values:
+        kmeans = KMeans(n_clusters=k, random_state=42, n_init=10)
+        cluster_label = kmeans.fit_predict(level_embeddings)
+        silhouette_avg = silhouette_score(level_embeddings, cluster_label)
+        silhouette_scores.append(silhouette_avg)
+    
+    optimal_k = k_values[np.argmax(silhouette_scores)]
+    return optimal_k
+
+def kmeans_clustering(optimal_k, embedded_documents):
+    kmeans = KMeans(n_clusters=optimal_k, random_state=42, n_init=10)
+    cluster_labels = kmeans.fit_predict(embedded_documents)
+    # centroids = kmeans.cluster_centers_
+    return cluster_labels, embedded_documents
+
+def Fetch_Each_clustering_Content_By_File(optimal_k, texts, cluster_labels):
     for cluster_label in range(optimal_k):
         cluster_texts = np.array(texts)[np.array(cluster_labels) == cluster_label]
         save_path = f'../txt/w7/cluster_{cluster_label}_texts.txt'
@@ -25,3 +46,11 @@ def Fetch_Each_clustering_Content(optimal_k, texts, cluster_labels):
                 file.write(text + '\n')
     
     return cluster_labels
+
+def Fetch_Each_clustering_Content_By_ListofArray(optimal_k, texts, cluster_labels):
+    clusters = [[] for _ in range(optimal_k)]  
+
+    for text, label in zip(texts, cluster_labels):
+        clusters[label].append(text)  
+    
+    return clusters
